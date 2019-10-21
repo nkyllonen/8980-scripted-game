124cd517de4220c904471e8e7f6f04f7d79624c0
Nikki Kyllonen <nikki-kyllonen@comcast.net>
2019-10-20 21:04:40 -0500

    displaying 4x4 board using tileItems strings

--- GEFS/MemoryGame/main.lua
+++ GEFS/MemoryGame/main.lua
@@ -6,7 +6,7 @@ print("Starting Lua for Memory Game")
 
 require "CommonLibs/vec3"
 
-CameraPosX = -5.0
+CameraPosX = -6.0
 CameraPosY = 1.0
 CameraPosZ = 0.0
 
@@ -25,6 +25,8 @@ CameraUp = vec3(CameraUpX, CameraUpY, CameraUpZ)
 animatedModels = {}
 velModel = {}
 rotYVelModel = {}
+
+boardSize = 4
 
 function frameUpdate(dt)
   for modelID,v in pairs(animatedModels) do
@@ -59,58 +61,92 @@ end
 -- arrays containing world objects
 tiles = {}
 tileItems = {}
+items = {"Tower", "Crystal", "Pug", "Sheep", "Ship", "Poplar", "Bottle", "Carrot"}
+
+function initializeBoard()
+  -- fill tileItems, 2 at a time
+  local idx = 1
+  for i = 1, boardSize*boardSize, 2 do
+    tileItems[i] = items[idx]
+    tileItems[i+1] = items[idx]
+    idx = idx + 1
+  end
+
+  -- TODO: randomize order of tileItems
+
+  -- fill tiles according to tileItems
+  local y = -0.8
+  local z = -1.8
+  for i = 1, boardSize*boardSize do
+    -- if past end of row, move up to next row
+    if z > 1.8 then
+      y = y + 1.2
+      z = -1.8
+    end
+
+    tiles[i] = addModel(tileItems[i] .. "Tile", 0, y, z)
+    animatedModels[tiles[i]] = true
+    rotYVelModel[tiles[i]] = 1
+
+    print(tileItems[i] .. " @ (0, " .. y .. " , " .. z .. ")")
+    z = z + 1.2
+  end
+
+end
+
+initializeBoard()
 
 -- bottom row
-idx = 1
-y = 0
-for z = -1.8, 1.8, 1.2 do
-  if idx % 2 == 0 then
-    tiles[idx] = addModel("PoplarTile", 0, y, z)
-    tileItems[idx] = "Poplar"
-  else
-    tiles[idx] = addModel("BottleTile", 0, y, z)
-    tileItems[idx] = "Bottle"
-  end
+-- idx = 1
+-- y = 0
+-- for z = -1.8, 1.8, 1.2 do
+--   if idx % 2 == 0 then
+--     tiles[idx] = addModel("PoplarTile", 0, y, z)
+--     tileItems[idx] = "Poplar"
+--   else
+--     tiles[idx] = addModel("BottleTile", 0, y, z)
+--     tileItems[idx] = "Bottle"
+--   end
 
-  -- rotate models around y-axis
-  animatedModels[tiles[idx]] = true
-  rotYVelModel[tiles[idx]] = 1
+--   -- rotate models around y-axis
+--   animatedModels[tiles[idx]] = true
+--   rotYVelModel[tiles[idx]] = 1
 
-  idx = idx + 1
-end
+--   idx = idx + 1
+-- end
 
--- middle row
-y = y + 1.2
-for z = -1.8, 1.8, 1.2 do
-  if idx % 2 == 0 then
-    tiles[idx] = addModel("CarrotTile", 0, y, z)
-    tileItems[idx] = "Carrot"
-  else
-    tiles[idx] = addModel("ShipTile", 0, y, z)
-    tileItems[idx] = "Ship"
-  end
+-- -- middle row
+-- y = y + 1.2
+-- for z = -1.8, 1.8, 1.2 do
+--   if idx % 2 == 0 then
+--     tiles[idx] = addModel("CarrotTile", 0, y, z)
+--     tileItems[idx] = "Carrot"
+--   else
+--     tiles[idx] = addModel("ShipTile", 0, y, z)
+--     tileItems[idx] = "Ship"
+--   end
 
-  -- rotate models around y-axis
-  animatedModels[tiles[idx]] = true
-  rotYVelModel[tiles[idx]] = 1
+--   -- rotate models around y-axis
+--   animatedModels[tiles[idx]] = true
+--   rotYVelModel[tiles[idx]] = 1
 
-  idx = idx + 1
-end
+--   idx = idx + 1
+-- end
 
--- top row
-y = y + 1.2
-for z = -1.8, 1.8, 1.2 do
-  if idx % 2 == 0 then
-    tiles[idx] = addModel("SheepTile", 0, y, z)
-    tileItems[idx] = "Sheep"
-  else
-    tiles[idx] = addModel("PugTile", 0, y, z)
-    tileItems[idx] = "Pug"
-  end
+-- -- top row
+-- y = y + 1.2
+-- for z = -1.8, 1.8, 1.2 do
+--   if idx % 2 == 0 then
+--     tiles[idx] = addModel("SheepTile", 0, y, z)
+--     tileItems[idx] = "Sheep"
+--   else
+--     tiles[idx] = addModel("PugTile", 0, y, z)
+--     tileItems[idx] = "Pug"
+--   end
 
-  -- rotate models around y-axis
-  animatedModels[tiles[idx]] = true
-  rotYVelModel[tiles[idx]] = 1
+--   -- rotate models around y-axis
+--   animatedModels[tiles[idx]] = true
+--   rotYVelModel[tiles[idx]] = 1
 
-  idx = idx + 1
-end
+--   idx = idx + 1
+-- end
